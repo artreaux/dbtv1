b068ed28f0334032b923099afeaeac16c0ff3aed
Lucas Sethary <resarijian@gmail.com>
2025-12-02 01:21:47 +0800

    updated concat sql

--- dbtv1/models/demo/concat.sql
+++ dbtv1/models/demo/concat.sql
@@ -1,5 +1,5 @@
 with concat_data as (
-    select * from {{ source('EMPLOYEE_DATA','EMPLOYEE_DATA_SHEET')}}
+    select * from {{ source('employee_data_sheet','EMPLOYEE_DATA')}}
 ),
 final as (
     select EMP_ID, CONCAT(EMP_FIRST_NAME, EMP_LAST_NAME) as EMP_FULL_NAME from concat_data
