30e4b4979dcf3b7ec7ea5b458b6d127060a16aa1
Lucas Sethary <resarijian@gmail.com>
2025-12-02 01:48:23 +0800

    adjusted casing

--- dbtv1/models/demo/concat.sql
+++ dbtv1/models/demo/concat.sql
@@ -1,5 +1,5 @@
 with concat_data as (
-    select * from {{ source('employee_data_sheet','EMPLOYEE_DATA')}}
+    select * from {{ source('EMPLOYEE_DATA_SHEET','EMPLOYEE_DATA')}}
 ),
 final as (
     select EMP_ID, CONCAT(EMP_FIRST_NAME, EMP_LAST_NAME) as EMP_FULL_NAME from concat_data
